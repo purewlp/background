da7f7adf8c2d6f8f6f5c9bb754e854e62ee8d788
wlp <112996920+purewlp@users.noreply.github.com>
2023-05-17 14:49:09 +0800

    注册登录

--- user/models.py
+++ user/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    videoNum = models.IntegerField(default=0)
+    likeNum = models.IntegerField(default=0)
+    collectNum = models.IntegerField(default=0)
+    fanNum = models.IntegerField(default=0)
+    followNum = models.IntegerField(default=0)
+    avatar = models.FileField(upload_to='avatar/')
+    createdTime = models.TimeField()
--- user/views.py
+++ user/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.utils import timezone
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+from user.models import User
+
+
+@csrf_exempt    # 跨域设置
+def register(request):  # 继承请求类
+    if request.method == 'POST':
+        username = request.POST.get('username')  # 获取请求体中的请求数据
+        password = request.POST.get('password')
+        if(User.objects.get(username= username)):
+            return JsonResponse({'message': "用户已存在"},status=401)
+        else:
+            newUser = User(username=username, password=password,createdTime=timezone.now())
+            newUser.save()
+            return JsonResponse({'msg': "注册成功"},status=200)
+    else:
+        return JsonResponse({'msg': "未知错误"},status=402)
+
+@csrf_exempt  # 跨域设置
+def login(request):  # 继承请求类
+     if request.method == 'get':
+        username = request.GET.get('username')  # 获取请求体中的请求数据
+        password = request.GET.get('password')
+        user=User.objects.get(username=username)
+        if(user== None):
+            return JsonResponse({'msg':'用户不存在'},status=401)
+        elif(password!=user.password):
+            return JsonResponse({'msg':'密码错误'},status=402)
+        else:
+            userData = {
+                'id':user.id,
+                'username': user.username,
+                'password': user.password,
+                'videoNum': user.videoNum,
+                'likeNum': user.likeNum,
+                'collectNum': user.collectNum,
+                'fanNum': user.fanNum,
+                'followNum': user.followNum,
+                'avatar': user.avatar,
+                'isSuperAdmin': user.is_superuser,
+                'createdTime': user.createdTime
+            }
+            return JsonResponse({'message': '登录成功','userData' : userData}, status=200)
+     else:
+        return JsonResponse({'msg': "未知错误"},status=402)
